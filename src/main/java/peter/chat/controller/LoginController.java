d481f3f497476ed0d1c53af234bd20adc0e0de40
peter <97747863+wcorn@users.noreply.github.com>
2023-07-06 14:00:03 +0900

    feature: 로그인 화면(thymeleaf)

---
+++ src/main/java/peter/chat/controller/LoginController.java
@@ -0,0 +1,12 @@
+package peter.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "/login";
+    }
+}
